28a11119a76eb2ff4e5a29f887e5c1c17e245219
ouazizi <158299018+helouazizi@users.noreply.github.com>
2024-10-20 20:14:24 +0100

    serv statis folders

--- back-end/main.go
+++ back-end/main.go
@@ -8,13 +8,21 @@ import (
 	"net/http"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//log.Printf("Received request: %s %s", r.Method, r.URL.Path) // Log the request method and path
+		next.ServeHTTP(w, r)                                        // Call the next handler in the chain
+	})
+}
+
 func main() {
-	http.HandleFunc("/", handler.HomeHandler)
-	http.HandleFunc("/contact", handler.ContactHandler)
-	http.HandleFunc("/account", handler.AccountHandler)
-	http.HandleFunc("/signin", handler.SignInHandler)
-	http.HandleFunc("/about", handler.AboutHandler)
-	http.HandleFunc("/css/", handler.CssHandler)
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(handler.HomeHandler)))
+	http.Handle("/contact", loggingMiddleware(http.HandlerFunc(handler.ContactHandler)))
+	http.Handle("/account", loggingMiddleware(http.HandlerFunc(handler.AccountHandler)))
+	http.Handle("/sign_in", loggingMiddleware(http.HandlerFunc(handler.SignInHandler)))
+	http.Handle("/cart", loggingMiddleware(http.HandlerFunc(handler.CartHandler)))
+	http.Handle("/about", loggingMiddleware(http.HandlerFunc(handler.AboutHandler)))
+	http.Handle("/css/style.css", loggingMiddleware(http.HandlerFunc(handler.CssHandler)))
 	fmt.Println("server listnning  on port 8080 >>> http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
